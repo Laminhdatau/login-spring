081b810070dc64b094f0dbf79f03f7f1cb801e6b
Laminhdatau <Laminhdataulamin2@gmail.com>
2022-10-18 10:18:10 +0800

    projek spring

---
+++ src/main/java/id/ac/poligon/BelajarSpringApplication.java
@@ -0,0 +1,13 @@
+package id.ac.poligon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/poligon/BelajarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.poligon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
